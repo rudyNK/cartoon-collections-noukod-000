d9da878387f41f1c8858d6c51b5884cdc348b6ab
Rudy Ravelin <rudy@noukod.com>
2018-09-20 20:00:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,30 @@
 # all my Arrays are here
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
 
 def roll_call_dwarves(array)
   # Your code here
+  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
   i = 0
    while i < array.length
      puts "#{i + 1}. #{array[i]}"
      i += 1
    end
 end
+roll_call_dwarves(dwarves) 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  nu_array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
 end
+
+summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls# code an argument here
   # Your code here
