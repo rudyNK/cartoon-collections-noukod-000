13d869308ab080ed5a9a39ae37d6aefadb14bc3f
Rudy Ravelin <rudy@noukod.com>
2018-09-20 20:02:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,11 +26,24 @@ end
 
 summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+long_planeteer_calls(call_screams)
+
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
+
+find_the_cheese(snacks)
+
